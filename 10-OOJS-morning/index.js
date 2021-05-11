0d6022119798586a74cbf32bffc5a4095547ef8a
ixnp <enprocopios@gmail.com>
2021-05-11 11:47:00 -0700

    oojs

---
+++ 10-OOJS-morning/index.js
@@ -0,0 +1,128 @@
+// let garfield = {
+//     name: "Garfield",
+//     breed: "Persian Tabby",
+//     introduce: function(){
+//         return `Meow, my name is Garfield and I'm a Persian Tabby.`
+//     }
+// }
+
+// let helloKitty = {
+//     name: "Hello Kitty",
+//     breed: "Cartoon",
+//     introduce: function(){
+//         return `Meow, my name is Hello Kitty and I'm a Cartoon.`
+//     }
+// }
+
+// let keyboardCat = {
+//     name: "Keyboard Cat",
+//     bread: "Yes",
+//     introduce: function(){
+//         return `Meow, my name is Keyboard Cat and I like Bread.`
+//     }
+// }
+
+// function Cat(name, age, color, breed){
+//     this.name = name
+//     this.age = age
+//     this.color = color
+//     this.breed = breed
+//     console.log(this)
+// }
+
+class Animal {
+  constructor(name, age, color, breed, image, species) {
+    this.species = species;
+    this.name = name;
+    this.age = age;
+    this.color = color;
+    this.breed = breed;
+    this.image = image;
+  }
+  isAnimal() {
+    return true;
+  }
+}
+class Dog extends Animal {
+  constructor(name, age, color, breed, image, species) {
+    super(species, name, age, color, breed, image);
+    this.favBall = [];
+  }
+}
+
+class Cat extends Animal {
+  constructor(name, age, color, breed, image, species) {
+    super(species, name, age, color, breed, image);
+    this.favFood = [];
+  }
+  sayHi() {
+    return `Meow, I\'m ${this.name}`;
+  }
+  addFood(food) {
+    this.favFood = [...this.favFood, food];
+  }
+
+  loopThroughFoods() {
+    console.log(this);
+    this.favFood.forEach((food) => console.log(`${this.name} loves ${food}`));
+  }
+  // logFoods(food){
+  //     console.log(this)
+  //     console.log(`${this.name} loves ${food}`)
+  // }
+
+  loopThroughFoodsArrow() {
+    this.favFood.forEach(this.logFoodsArrow);
+  }
+  logFoodsArrow = (food) => console.log(`${this.name} loves ${food}`);
+}
+
+const rose = new Cat(
+  "rose",
+  9,
+  "tuxedo",
+  "domestic longhair",
+  "./assets/gm_cat.png",
+  "Felis catus"
+);
+const luke = new Cat(
+  "luke",
+  3,
+  "white and brown",
+  "domestic longhair",
+  "./assets/luke.png",
+  "Felis catus"
+);
+const marshmallow = new Cat(
+  "marshmallow",
+  5,
+  "marshmallow",
+  "ragdoll",
+  "./assets/marshmallow.png",
+  "Felis catus"
+);
+//console.log(rose.sayHi())
+// console.log(luke.name)
+// console.log(marshmallow)
+rose.addFood("fish");
+rose.addFood("chicken");
+rose.addFood("plants");
+rose.addFood("ix's food");
+//console.log(rose.favFood)
+
+// let {name, age, likesMice} = luke
+// console.log(name, age, likesMice)
+let { age, name, image } = luke;
+console.log(name, age, image);
+
+function birthDay({ age, name }) {
+  console.log(`Happy Birthday ${name} you are ${age} old!`);
+}
+birthDay(rose);
+
+let [food1, food2] = rose.favFood;
+console.log(food1, food2);
+
+let [a, b] = ["red", "blue"];
+console.log(a);
+console.log(b);
